dbf0c5f3b8c886db75f917eb9103ea7833417765
mkellner43 <matthew.kellner@outlook.com>
2023-02-01 18:56:02 -0600

    updated default input

--- src/components/InfoLable.js
+++ src/components/InfoLable.js
@@ -4,7 +4,7 @@ class InfoLable extends Component {
   constructor(props){
     super(props)
     this.state= {
-      text: props.value,
+      text: '',
       isEditing: false,
       tag: props.tag,
       default: props.value
